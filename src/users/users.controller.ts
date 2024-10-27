bb0797d720beb7d1fa387f966a24fb11b1b49425
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-27 20:53:21 +0100

    Parsing URL params

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get()
   getUsers(): any {
     return [{ id: 0 }];
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): any {
+    return { id };
   }
 }

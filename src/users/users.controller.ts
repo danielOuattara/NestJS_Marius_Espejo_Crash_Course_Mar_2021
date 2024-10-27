3e9a04c34a70de93eaedd60bb1ef227a6166df52
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-27 22:19:00 +0100

    Swagger OpenAPI Docs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,21 +2,31 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
+  @ApiOkResponse({ type: User, isArray: true })
   @Get()
   getUsers(): User[] {
     return this.userService.getUsers();
   }
 
+  @ApiOkResponse({ type: User })
   @Get(':id')
   getUserById(@Param('id') id: string): User {
     return this.userService.getUserById(+id);
   }
 
+  @ApiCreatedResponse({
+    type: User,
+    isArray: true,
+    description:
+      'return an array of all users with the new created user among them',
+  })
   @Post()
   createUser(@Body() body: CreateUserDto): User[] {
     return this.userService.createUser(body);

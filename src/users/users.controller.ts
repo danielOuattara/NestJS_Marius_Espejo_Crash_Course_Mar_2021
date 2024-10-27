ee72e140ed925dad23143e8a110ad7ee373ee583
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-27 21:36:13 +0100

    Dependency Injection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
   @Get()
   getUsers(): any {
-    return [{ id: 0 }];
+    return this.userService.getUsers();
   }
 
   @Get(':id')
   getUserById(@Param('id') id: string): any {
-    return { id };
+    return this.userService.getUserById(+id);
+  }
+
+  @Post()
+  createUser(@Body() body: any): any {
+    return this.userService.createUser(body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: any = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Ann' },
+    { id: 3, name: 'Mike' },
+  ];
+
+  getUsers(): any {
+    return this.users;
+  }
+
+  getUserById(id: number): any {
+    return this.users.find((user) => user.id === id);
+  }
+
+  createUser(name: string): any[] {
+    this.users = [...this.users, { id: new Date().getTime(), name }];
+    return this.getUsers();
+  }
+}
